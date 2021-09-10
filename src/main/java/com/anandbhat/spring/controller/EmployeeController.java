f98f46b5d5ceb74a3c1753af81309c09506e4852
Anand Bhat <62979516+AnandBhat-jpeg@users.noreply.github.com>
2021-09-10 22:22:10 +0530

    InitialCommit

---
+++ src/main/java/com/anandbhat/spring/controller/EmployeeController.java
@@ -0,0 +1,57 @@
+package com.anandbhat.spring.controller;
+
+
+import com.anandbhat.spring.exception.ResourceNotFoundException;
+import com.anandbhat.spring.model.Employee;
+import com.anandbhat.spring.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/v1")
+public class EmployeeController {
+
+    private static final String RESOURCE_NOT_FOUND = "Employee Not found for the id :";
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    //  Creating the  get Methods
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable(value = "id") long employeeId) throws ResourceNotFoundException {
+        Employee employee = employeeRepository.findById(employeeId).orElseThrow(() -> new ResourceNotFoundException("Employee Not found for the id :" + employeeId));
+        return ResponseEntity.ok().body(employee);
+    }
+
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable(value = "id") long id, @RequestBody Employee employeeDetails) throws ResourceNotFoundException {
+        Employee employee = employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(RESOURCE_NOT_FOUND + id));
+        employee.setFirstName(employeeDetails.getFirstName());
+        employee.setLastName(employeeDetails.getLastName());
+        employee.setEmailId(employeeDetails.getEmailId());
+        employeeRepository.save(employee);
+        return ResponseEntity.ok().body(employee);
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public ResponseEntity<?> deleteEmployee(@PathVariable(value = "id") long id) throws ResourceNotFoundException {
+        employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(RESOURCE_NOT_FOUND + id));
+        employeeRepository.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+
+
+}
---
+++ src/main/java/com/anandbhat/spring/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.anandbhat.spring.repository;
+
+import com.anandbhat.spring.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
